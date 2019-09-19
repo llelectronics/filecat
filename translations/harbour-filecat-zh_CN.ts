329fa51b7a701b2bfdbd019588920f072f25856e
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-09-19 10:50:09 +0800

    Create harbour-filecat-zh_CN.ts

---
+++ translations/harbour-filecat-zh_CN.ts
@@ -0,0 +1,324 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="components_gallery-la-image-loading-failed">
+        <source>Oops, can&apos;t display the image</source>
+        <extracomment>Image loading failed</extracomment>
+        <translation>抱歉，无法显示图片</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>我的封面</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirDialog</name>
+    <message>
+        <source>Error creating new directory</source>
+        <translation>创建新目录出错</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>创建文件夹</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>创建</translation>
+    </message>
+    <message>
+        <source>Create a new folder under</source>
+        <translation>创建新文件夹于</translation>
+    </message>
+    <message>
+        <source>Folder name</source>
+        <translation>文件夹名称</translation>
+    </message>
+</context>
+<context>
+    <name>FileProperties</name>
+    <message>
+        <source>Change Permissions</source>
+        <translation>修改权限</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <source>Error renaming</source>
+        <translation>重命名出错</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>属性</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <source>Link to %1</source>
+        <translation>链接到 %1</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <source>Permissions</source>
+        <translation>权限</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>所属者</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>组</translation>
+    </message>
+    <message>
+        <source>Last modified</source>
+        <translation>最近修改</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <source>Opening...</source>
+        <translation>正在打开……</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>创建文件夹</translation>
+    </message>
+    <message>
+        <source>Add to places</source>
+        <translation>添加到位置</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>属性</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <source>directory</source>
+        <translation>目录</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>剪切</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <source>Error pasting file </source>
+        <translation>粘贴文件出错</translation>
+    </message>
+    <message>
+        <source>File operation succeeded</source>
+        <translation>文件操作成功</translation>
+    </message>
+</context>
+<context>
+    <name>OpenURLPage</name>
+    <message>
+        <source>Load URL</source>
+        <translation>加载 URL</translation>
+    </message>
+    <message>
+        <source>Type in URL here</source>
+        <translation><在此输入 URL/translation>
+    </message>
+    <message>
+        <source>URL to media file/stream</source>
+        <translation>媒体文件URL </translation>
+    </message>
+</context>
+<context>
+    <name>PermissionDialog</name>
+    <message>
+        <source>Could not change permission</source>
+        <translation>无法修改权限</translation>
+    </message>
+    <message>
+        <source>Change Permissions</source>
+        <translation>修改权限</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>修改</translation>
+    </message>
+    <message>
+        <source>Change permissions for</source>
+        <translation>修改权限</translation>
+    </message>
+    <message>
+        <source>Read</source>
+        <translation>可读</translation>
+    </message>
+    <message>
+        <source>Write</source>
+        <translation>可写</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>可执行</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>所属者</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>组</translation>
+    </message>
+    <message>
+        <source>Others</source>
+        <translation>其它</translation>
+    </message>
+</context>
+<context>
+    <name>PlacesPage</name>
+    <message>
+        <source>Device memory</source>
+        <translation>设备存储</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>SD卡</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>主页</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>文档</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>下载</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>音乐</translation>
+    </message>
+    <message>
+        <source>Pictures</source>
+        <translation>图片</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>视频</translation>
+    </message>
+    <message>
+        <source>Places</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation>设备</translation>
+    </message>
+    <message>
+        <source>Common</source>
+        <translation>通用</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>正在删除</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>重命名</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <source>Rename</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <source>Give a new name for</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <source>New name</source>
+        <translation>新名称</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Open File</source>
+        <translation>打开文件</translation>
+    </message>
+    <message>
+        <source>Open Stream</source>
+        <translation>打开流</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayerComponent</name>
+    <message>
+        <source>Pull down to load file or stream</source>
+        <translation>下拉以加载文件或流媒体</translation>
+    </message>
+    <message>
+        <source>Dismiss</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to a problem allocating resources.
+On Youtube Videos please make sure to be logged in. Some videos might be geoblocked or require you to be logged into youtube.</source>
+        <translation>由于资源分配问题，视频无法播放。请务必登录以播放 YouTube 网站上的视频。有些视频可能被地理锁定，或者需要你登录到 youtube。</translation>
+    </message>
+    <message>
+        <source>
+The audio and or video format is not supported.</source>
+        <translation>不支持的音频及视频格式。</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to network issues.</source>
+        <translation>因网络问题无法播放视频</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to insufficient permissions.</source>
+        <translation>权限不足，无法播放视频</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played because the media service could not be instantiated.</source>
+        <translation>因无法实例化媒体服务，无法播放视频。</translation>
+    </message>
+</context>
+</TS>
