a47289033df12ac853a8373b72fcf4fbe612f1f3
Leszek Lesner <leszek.lesner@web.de>
2019-09-25 15:59:25 +0200

    Add greek translation

---
+++ translations/harbour-filecat-el.ts
@@ -0,0 +1,388 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name></name>
+    <message id="components_gallery-la-image-loading-failed">
+        <source>Oops, can&apos;t display the image</source>
+        <extracomment>Image loading failed</extracomment>
+        <translation>Ωχ, δεν είναι δυνατή η εμφάνιση της εικόνας</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirDialog</name>
+    <message>
+        <source>Error creating new directory</source>
+        <translation>Σφάλμα δημιουργίας καταλόγου</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>Δημιουργία Φακέλου</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Δημιουργία</translation>
+    </message>
+    <message>
+        <source>Create a new folder under</source>
+        <translation>Δημιουργία νέου φακέλου σε</translation>
+    </message>
+    <message>
+        <source>Folder name</source>
+        <translation>Όνομα φακέλου</translation>
+    </message>
+</context>
+<context>
+    <name>FileItemDelegate</name>
+    <message>
+        <source>Deleting </source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Αποκοπή</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Αντιγραφή</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Ιδιότητες</translation>
+    </message>
+</context>
+<context>
+    <name>FileProperties</name>
+    <message>
+        <source>Change Permissions</source>
+        <translation>Αλλαγή Δικαιωμάτων</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Μετονομασία</translation>
+    </message>
+    <message>
+        <source>Error renaming</source>
+        <translation>Σφάλμα κατά την μετονομασία</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Τελευταία αλλαγή</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Τοποθεσία</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Τύπος</translation>
+    </message>
+    <message>
+        <source>Link to %1</source>
+        <translation>Σύνδεση στο %1</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Μέγεθος</translation>
+    </message>
+    <message>
+        <source>Permissions</source>
+        <translation>Δικαιώματα</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Ιδιοκτήτης</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Ομάδα</translation>
+    </message>
+    <message>
+        <source>Last modified</source>
+        <translation>Τελευταία αλλαγή</translation>
+    </message>
+    <message>
+        <source>Open with Texteditor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <source>Opening...</source>
+        <translation>Άνοιγμα...</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>Δημιουργία Φακέλου</translation>
+    </message>
+    <message>
+        <source>Add to places</source>
+        <translation>Προσθήκη στα μέρη</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Επικόληση</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Ιδιότητες</translation>
+    </message>
+    <message>
+        <source>Error pasting file </source>
+        <translation>Σφάλμα επικόλησης αρχείου</translation>
+    </message>
+    <message>
+        <source>File operation succeeded</source>
+        <translation>Η λειτουργία αρχείου επιτεύχθηκε</translation>
+    </message>
+    <message>
+        <source>Hide hidden files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Μέγεθος</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Τύπος</translation>
+    </message>
+    <message>
+        <source>Sort by: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saved directory to last known</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpenURLPage</name>
+    <message>
+        <source>Load URL</source>
+        <translation>Φόρτωση διεύθυνσης URL</translation>
+    </message>
+    <message>
+        <source>Type in URL here</source>
+        <translation>Πληκτρολογήστε τη διεύθυνση URL εδώ</translation>
+    </message>
+    <message>
+        <source>URL to media file/stream</source>
+        <translation>Διεύθυνση URL αρχείου πολυμέσων / ροής</translation>
+    </message>
+</context>
+<context>
+    <name>PermissionDialog</name>
+    <message>
+        <source>Could not change permission</source>
+        <translation>Αδυναμία αλλαγής δικαιωμάτων</translation>
+    </message>
+    <message>
+        <source>Change Permissions</source>
+        <translation>Αλλαγή Δικαιωμάτων</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>Αλλαγή</translation>
+    </message>
+    <message>
+        <source>Change permissions for</source>
+        <translation>Αλλαγή δικαιωμάτων για</translation>
+    </message>
+    <message>
+        <source>Read</source>
+        <translation>Ανάγνωση</translation>
+    </message>
+    <message>
+        <source>Write</source>
+        <translation>Εγγραφή</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Εκτέλεση</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Ιδιοκτήτης</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Ομάδα</translation>
+    </message>
+    <message>
+        <source>Others</source>
+        <translation>Άλλα</translation>
+    </message>
+</context>
+<context>
+    <name>PlacesPage</name>
+    <message>
+        <source>Device memory</source>
+        <translation>Μνήμη συσκευής</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>Κάρτα SD</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>Έγγραφα</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>Λήψεις</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>Μουσική</translation>
+    </message>
+    <message>
+        <source>Pictures</source>
+        <translation>Εικόνες</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>Βίντεο</translation>
+    </message>
+    <message>
+        <source>Android Storage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Places</source>
+        <translation>Μέρη</translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation>Συσκευές</translation>
+    </message>
+    <message>
+        <source>Common</source>
+        <translation>Κοινά</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Προσαρμοσμένο</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>Διαγραφή</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Μετονομασία</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <source>Rename</source>
+        <translation>Μετονομασία</translation>
+    </message>
+    <message>
+        <source>Give a new name for</source>
+        <translation>Δώστε ένα νέο όνομα για</translation>
+    </message>
+    <message>
+        <source>New name</source>
+        <translation>Νέο όνομα</translation>
+    </message>
+</context>
+<context>
+    <name>TextEditor</name>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Open File</source>
+        <translation>Άνοιγμα Αρχείου</translation>
+    </message>
+    <message>
+        <source>Open Stream</source>
+        <translation>Άνοιγμα Ροής</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayerComponent</name>
+    <message>
+        <source>Pull down to load file or stream</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dismiss</source>
+        <translation>Απόριψη</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to a problem allocating resources.
+On Youtube Videos please make sure to be logged in. Some videos might be geoblocked or require you to be logged into youtube.</source>
+        <translation>
+Το βίντεο δεν μπορεί να αναπαραχθεί εξαιτίας ενός προβλήματος κατανομής πόρων.
+Στα Βίντεο Youtube, βεβαιωθείτε ότι έχετε συνδεθεί. Ορισμένα βίντεο ενδέχεται να είναι μπλοκαρισμένα γεωγραφικά ή απαιτούν να συνδεθείτε στο youtube.</translation>
+    </message>
+    <message>
+        <source>
+The audio and or video format is not supported.</source>
+        <translation>
+Η μορφή ήχου ή βίντεο δεν υποστηρίζεται.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to network issues.</source>
+        <translation>
+Το βίντεο δεν μπορεί να αναπαραχθεί λόγω προβλημάτων δικτύου.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to insufficient permissions.</source>
+        <translation>
+Το βίντεο δεν μπορεί να αναπαραχθεί λόγω ανεπαρκών δικαιωμάτων.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played because the media service could not be instantiated.</source>
+        <translation>
+Δεν είναι δυνατή η αναπαραγωγή του βίντεο επειδή η υπηρεσία πολυμέσων δεν μπορεί να εκτελεστεί.</translation>
+    </message>
+    <message>
+        <source>Zoomed to fit screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Original</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
