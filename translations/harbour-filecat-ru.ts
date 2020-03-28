61a3b2458d4e796c141293cb3181e0601c6cd95b
Вячеслав Диконов <sdiconov@mail.ru>
2020-03-29 01:05:47 +0300

    Russian translation

---
+++ translations/harbour-filecat-ru.ts
@@ -0,0 +1,406 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="components_gallery-la-image-loading-failed">
+        <source>Oops, can&apos;t display the image</source>
+        <extracomment>Image loading failed</extracomment>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>Менеджер файлов</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDirDialog</name>
+    <message>
+        <source>Error creating new directory</source>
+        <translation>Ошибка при создании каталога</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>Новая папка</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Создать</translation>
+    </message>
+    <message>
+        <source>Create a new folder under</source>
+        <translation>Создать папку в</translation>
+    </message>
+    <message>
+        <source>Folder name</source>
+        <translation>Имя папки</translation>
+    </message>
+</context>
+<context>
+    <name>FileItemDelegate</name>
+    <message>
+        <source>Deleting </source>
+        <translation>Удаление</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Вырезать</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Свойства</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Выделить</translation>
+    </message>
+</context>
+<context>
+    <name>FileProperties</name>
+    <message>
+        <source>Change Permissions</source>
+        <translation>Изменить права</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <source>Error renaming</source>
+        <translation>Ошибка при переименовании</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Свойства</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Местоположение</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <source>Link to %1</source>
+        <translation>Ссылка на %1</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <source>Permissions</source>
+        <translation>Права доступа</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Владелец</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Группа</translation>
+    </message>
+    <message>
+        <source>Last modified</source>
+        <translation>Изменено</translation>
+    </message>
+    <message>
+        <source>Open with Texteditor</source>
+        <translation>Открыть редактором текстов</translation>
+    </message>
+</context>
+<context>
+    <name>MultiSelectBar</name>
+    <message>
+        <source>Deleting %1 files</source>
+        <translation>Удаление %1 файлов</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <source>Opening...</source>
+        <translation>Открытие...</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>Создать папку</translation>
+    </message>
+    <message>
+        <source>Add to places</source>
+        <translation>Добавить в закладки</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Свойства</translation>
+    </message>
+    <message>
+        <source>Error pasting file </source>
+        <translation>Ошибка при вставке файла</translation>
+    </message>
+    <message>
+        <source>File operation succeeded</source>
+        <translation>Файловая операция завершена</translation>
+    </message>
+    <message>
+        <source>Hide hidden files</source>
+        <translation>Прятать скрытые файлы</translation>
+    </message>
+    <message>
+        <source>Show hidden files</source>
+        <translation>Показывать скрытые файлы</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>имени</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>времени</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>размеру</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>типу</translation>
+    </message>
+    <message>
+        <source>Sort by: </source>
+        <translation>Сортировать по: </translation>
+    </message>
+    <message>
+        <source>Saved directory to last known</source>
+        <translation>Каталог сохранен в журнал</translation>
+    </message>
+    <message>
+        <source>Error deleting file(s)</source>
+        <translation>Ошибка при удалении файла</translation>
+    </message>
+    <message>
+        <source>File deletion succeeded</source>
+        <translation>Удаление файлов завершено</translation>
+    </message>
+    <message>
+        <source> Files</source>
+        <translation> Файлов</translation>
+    </message>
+</context>
+<context>
+    <name>OpenURLPage</name>
+    <message>
+        <source>Load URL</source>
+        <translation>Загрузить URL</translation>
+    </message>
+    <message>
+        <source>Type in URL here</source>
+        <translation>Введите URL</translation>
+    </message>
+    <message>
+        <source>URL to media file/stream</source>
+        <translation>URL медаифайла/потока</translation>
+    </message>
+</context>
+<context>
+    <name>PermissionDialog</name>
+    <message>
+        <source>Could not change permission</source>
+        <translation>Невозможно изменить права доступа</translation>
+    </message>
+    <message>
+        <source>Change Permissions</source>
+        <translation>Изменить права</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <source>Change permissions for</source>
+        <translation>Изменить права доступа для</translation>
+    </message>
+    <message>
+        <source>Read</source>
+        <translation>Чтение</translation>
+    </message>
+    <message>
+        <source>Write</source>
+        <translation>Запись</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Запуск</translation>
+    </message>
+    <message>
+        <source>Owner</source>
+        <translation>Владелец</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Группа</translation>
+    </message>
+    <message>
+        <source>Others</source>
+        <translation>Прочие</translation>
+    </message>
+</context>
+<context>
+    <name>PlacesPage</name>
+    <message>
+        <source>Device memory</source>
+        <translation>Внутренняя память</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>SD-карта</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Домашняя папка</translation>
+    </message>
+    <message>
+        <source>Documents</source>
+        <translation>Документы</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>Загрузки</translation>
+    </message>
+    <message>
+        <source>Music</source>
+        <translation>Музыка</translation>
+    </message>
+    <message>
+        <source>Pictures</source>
+        <translation>Изображения</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>Видео</translation>
+    </message>
+    <message>
+        <source>Android Storage</source>
+        <translation>Хранилище Android</translation>
+    </message>
+    <message>
+        <source>Places</source>
+        <translation>Места</translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation>Устройства</translation>
+    </message>
+    <message>
+        <source>Common</source>
+        <translation>Стандартные</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Личные</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>Удаление </translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <source>Give a new name for</source>
+        <translation>Задать новое имя для</translation>
+    </message>
+    <message>
+        <source>New name</source>
+        <translation>Новое имя</translation>
+    </message>
+</context>
+<context>
+    <name>TextEditor</name>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Open File</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <source>Open Stream</source>
+        <translation>Подключиться к потоку</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayerComponent</name>
+    <message>
+        <source>Pull down to load file or stream</source>
+        <translation>Потяните вниз, чтобы загрузить файл или поток</translation>
+    </message>
+    <message>
+        <source>Dismiss</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to a problem allocating resources.
+On Youtube Videos please make sure to be logged in. Some videos might be geoblocked or require you to be logged into youtube.</source>
+        <translation>Невозможно воспроизводить видео из-за проблем с выделением ресурсов.
+Убедитесь, что вы вошли в Youtube. Некоторые видеоролики могут иметь географические ограничения или требовать входа в систему youtube.</translation>
+    </message>
+    <message>
+        <source>
+The audio and or video format is not supported.</source>
+        <translation>Неизвестный формат аудио или видео.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to network issues.</source>
+        <translation>Невозможно воспроизводить видео из-за проблем с сетью</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played due to insufficient permissions.</source>
+        <translation>Невозможно воспроизводить видео, так как нет прав доступа.</translation>
+    </message>
+    <message>
+        <source>
+The video cannot be played because the media service could not be instantiated.</source>
+        <translation>Невозможно воспроизводить видео из-за проблем с запуском проигрывателя.</translation>
+    </message>
+    <message>
+        <source>Zoomed to fit screen</source>
+        <translation>Подгонять к размеру экрана</translation>
+    </message>
+    <message>
+        <source>Original</source>
+        <translation>Исходный размер</translation>
+    </message>
+</context>
+</TS>
